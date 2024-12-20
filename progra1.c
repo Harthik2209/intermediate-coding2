805aee56d2071a05a69b59bc00a5a646737b77e5
Harthik2209 <hboddeti@gitam.in>
2024-12-20 10:47:21 +0530

    Update progra1.c

--- progra1.c
+++ progra1.c
@@ -4,10 +4,10 @@ int removeKey(int arr[], int n, int key) {
     int count = 0;
     for (int i = 0; i < n; i++) {
         if (arr[i] != key) {
-            arr[count++] = arr[i]; // Shift elements not equal to key
+            arr[count++] = arr[i]; 
         }
     }
-    return count; // Return the count of remaining elements
+    return count; 
 }
 
 int main() {
